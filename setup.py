f204b27a7cd07d6ea7b514aad8c3f8d61de22098
SaaSkit <saaskit@DevBook.home>
2010-01-06 20:17:43 +0000

    License Fix

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(name="django-subscription",
            version="0.1",
            description="Subscriptions based web app using django-paypal",
-           author="CrowdSense",
-           author_email="admin@crowdsense.com",
+           author="SaaS kit",
+           author_email="admin@saaskit.org",
            packages=find_packages(),
            include_package_data=True,
 )
