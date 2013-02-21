c0ec7f7c3efc79a55a7faf974b79b3d5f0b0cd7e
Calvin Cheng <linchuan.cheng@gmail.com>
2013-02-21 14:22:34 +0800

    Override subscription model's save method to handle trial_unit == '0' scenario. That is, no trial unit.

--- subscription/models.py
+++ subscription/models.py
@@ -7,7 +7,9 @@ from django.utils.translation import ugettext as _, ungettext, ugettext_lazy
 
 from paypal.standard import ipn
 
-import signals, utils
+import signals
+import utils
+
 
 class Transaction(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
@@ -27,30 +29,30 @@ class Transaction(models.Model):
 
 
 _recurrence_unit_days = {
-    'D' : 1.,
-    'W' : 7.,
-    'M' : 30.4368,                      # http://en.wikipedia.org/wiki/Month#Julian_and_Gregorian_calendars
-    'Y' : 365.2425,                     # http://en.wikipedia.org/wiki/Year#Calendar_year
+    'D': 1.,
+    'W': 7.,
+    'M': 30.4368,                      # http://en.wikipedia.org/wiki/Month#Julian_and_Gregorian_calendars
+    'Y': 365.2425,                     # http://en.wikipedia.org/wiki/Year#Calendar_year
     }
 
-_TIME_UNIT_CHOICES=(
+_TIME_UNIT_CHOICES = (
+    ('0', ugettext_lazy('No trial')),
     ('D', ugettext_lazy('Day')),
     ('W', ugettext_lazy('Week')),
     ('M', ugettext_lazy('Month')),
     ('Y', ugettext_lazy('Year')),
     )
 
+
 class Subscription(models.Model):
     name = models.CharField(max_length=100, unique=True, null=False)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=64, decimal_places=2)
     trial_period = models.PositiveIntegerField(null=True, blank=True)
-    trial_unit = models.CharField(max_length=1, null=True,
-                                       choices = ((None, ugettext_lazy("No trial")),)
-                                       + _TIME_UNIT_CHOICES)
+    trial_unit = models.CharField(max_length=1, null=True, choices=_TIME_UNIT_CHOICES)
     recurrence_period = models.PositiveIntegerField(null=True, blank=True)
     recurrence_unit = models.CharField(max_length=1, null=True,
-                                       choices = ((None, ugettext_lazy("No recurrence")),)
+                                       choices=((None, ugettext_lazy("No recurrence")),)
                                        + _TIME_UNIT_CHOICES)
     group = models.ForeignKey(auth.models.Group, null=False, blank=False, unique=False)
 
@@ -62,9 +64,10 @@ class Subscription(models.Model):
         }
 
     class Meta:
-        ordering = ('price','-recurrence_period')
+        ordering = ('price', '-recurrence_period')
 
-    def __unicode__(self): return self.name
+    def __unicode__(self):
+        return self.name
 
     def price_per_day(self):
         """Return estimate subscription price per day, as a float.
@@ -77,44 +80,58 @@ class Subscription(models.Model):
         if self.recurrence_unit is None:
             return 0
         return float(self.price) / (
-            self.recurrence_period*_recurrence_unit_days[self.recurrence_unit]
+            self.recurrence_period * _recurrence_unit_days[self.recurrence_unit]
             )
 
     @models.permalink
     def get_absolute_url(self):
-        return ( 'subscription_detail', (), dict(object_id=str(self.id)) )
+        return ('subscription_detail', (), dict(object_id=str(self.id)))
 
     def get_pricing_display(self):
-        if not self.price: return u'Free'
+        if not self.price:
+            return u'Free'
         elif self.recurrence_period:
             return ungettext('%(price).02f / %(unit)s',
                              '%(price).02f / %(period)d %(unit_plural)s',
                              self.recurrence_period) % {
-                'price':self.price,
-                'unit':self.get_recurrence_unit_display(),
-                'unit_plural':_(self._PLURAL_UNITS[self.recurrence_unit],),
-                'period':self.recurrence_period,
+                'price': self.price,
+                'unit': self.get_recurrence_unit_display(),
+                'unit_plural': _(self._PLURAL_UNITS[self.recurrence_unit],),
+                'period': self.recurrence_period,
                 }
-        else: return _('%(price).02f one-time fee') % { 'price':self.price }
+        else:
+            return _('%(price).02f one-time fee') % {'price': self.price}
 
     def get_trial_display(self):
         if self.trial_period:
             return ungettext('One %(unit)s',
                              '%(period)d %(unit_plural)s',
                              self.trial_period) % {
-                'unit':self.get_trial_unit_display().lower(),
-                'unit_plural':_(self._PLURAL_UNITS[self.trial_unit],),
-                'period':self.trial_period,
-                }
+                'unit': self.get_trial_unit_display().lower(),
+                'unit_plural': _(self._PLURAL_UNITS[self.trial_unit],),
+                'period': self.trial_period,
+            }
         else:
             return _("No trial")
+
+    def save(self, *args, **kwargs):
+        """
+        Set trial period to 0 if the trial unit is 0
+        """
+        if self.trial_unit == "0":
+            self.trial_period = 0
+
+        super(Subscription, self).save(*args, **kwargs)
+
 
 # add User.get_subscription() method
 def __user_get_subscription(user):
     if not hasattr(user, '_subscription_cache'):
         sl = Subscription.objects.filter(group__in=user.groups.all())[:1]
-        if sl: user._subscription_cache = sl[0]
-        else: user._subscription_cache = None
+        if sl:
+            user._subscription_cache = sl[0]
+        else:
+            user._subscription_cache = None
     return user._subscription_cache
 auth.models.User.add_to_class('get_subscription', __user_get_subscription)
 
@@ -124,10 +141,11 @@ class ActiveUSManager(models.Manager):
     def get_query_set(self):
         return super(ActiveUSManager, self).get_query_set().filter(active=True)
 
+
 class UserSubscription(models.Model):
     user = models.ForeignKey(auth.models.User)
     subscription = models.ForeignKey(Subscription)
-    expires = models.DateField(null = True, default=datetime.date.today)
+    expires = models.DateField(null=True, default=datetime.date.today)
     active = models.BooleanField(default=True)
     cancelled = models.BooleanField(default=True)
 
@@ -138,7 +156,7 @@ class UserSubscription(models.Model):
         getattr(settings, 'SUBSCRIPTION_GRACE_PERIOD', 2))
 
     class Meta:
-        unique_together = ( ('user','subscription'), )
+        unique_together = (('user', 'subscription'), )
 
     def user_is_group_member(self):
         "Returns True is user is member of subscription's group"
@@ -149,7 +167,7 @@ class UserSubscription(models.Model):
         """Returns true if there is more than SUBSCRIPTION_GRACE_PERIOD
         days after expiration date."""
         return self.expires is not None and (
-            self.expires + self.grace_timedelta < datetime.date.today() )
+            self.expires + self.grace_timedelta < datetime.date.today())
     expired.boolean = True
 
     def valid(self):
@@ -157,8 +175,10 @@ class UserSubscription(models.Model):
 
         Returns True if not expired and user is in group, or expired
         and user is not in group."""
-        if self.expired() or not self.active: return not self.user_is_group_member()
-        else: return self.user_is_group_member()
+        if self.expired() or not self.active:
+            return not self.user_is_group_member()
+        else:
+            return self.user_is_group_member()
     valid.boolean = True
 
     def unsubscribe(self):
@@ -184,7 +204,8 @@ class UserSubscription(models.Model):
                     Transaction(user=self.user, subscription=self.subscription, ipn=None,
                                 event='remove subscription (expired)'
                                 ).save()
-            else: self.subscribe()
+            else:
+                self.subscribe()
 
     def extend(self, timedelta=None):
         """Extend subscription by `timedelta' or by subscription's
@@ -214,60 +235,70 @@ class UserSubscription(models.Model):
         reason to display.
         """
         if self.subscription == subscription:
-            if self.active and self.cancelled: return None # allow resubscribing
-            return [ _(u'This is your current subscription.') ]
+            if self.active and self.cancelled:
+                return None  # allow resubscribing
+            return [_(u'This is your current subscription.')]
         return [
             res[1]
             for res in signals.change_check.send(
                 self, subscription=subscription)
-            if res[1] ]
+            if res[1]]
 
     @models.permalink
     def get_absolute_url(self):
-        return ( 'subscription_usersubscription_detail',
-                 (), dict(object_id=str(self.id)) )
+        return ('subscription_usersubscription_detail', (), dict(object_id=str(self.id)))
 
     def __unicode__(self):
-        rv = u"%s's %s" % ( self.user, self.subscription )
+        rv = u"%s's %s" % (self.user, self.subscription)
         if self.expired():
             rv += u' (expired)'
         return rv
 
+
 def unsubscribe_expired():
     """Unsubscribes all users whose subscription has expired.
-
     Loops through all UserSubscription objects with `expires' field
     earlier than datetime.date.today() and forces correct group
     membership."""
     for us in UserSubscription.objects.get(expires__lt=datetime.date.today()):
         us.fix()
 
-#### Handle PayPal signals
 
+#### Handle PayPal signals
 def _ipn_usersubscription(payment):
     class PseudoUS(object):
         pk = None
-        def __nonzero__(self): return False
+
+        def __nonzero__(self):
+            return False
+
         def __init__(self, user, subscription):
             self.user = user
             self.subscription = subscription
 
-    try: s = Subscription.objects.get(id=payment.item_number)
-    except Subscription.DoesNotExist: s = None
+    try:
+        s = Subscription.objects.get(id=payment.item_number)
+    except Subscription.DoesNotExist:
+        s = None
 
-    try: u = auth.models.User.objects.get(id=payment.custom)
-    except auth.models.User.DoesNotExist: u = None
+    try:
+        u = auth.models.User.objects.get(id=payment.custom)
+    except auth.models.User.DoesNotExist:
+        u = None
 
     if u and s:
-        try: us = UserSubscription.objects.get(subscription=s, user=u)
+        try:
+            us = UserSubscription.objects.get(subscription=s, user=u)
         except UserSubscription.DoesNotExist:
             us = UserSubscription(user=u, subscription=s, active=False)
             Transaction(user=u, subscription=s, ipn=payment,
                         event='new usersubscription', amount=payment.mc_gross
                         ).save()
-    else: us = PseudoUS(user=u,subscription=s)
+    else:
+        us = PseudoUS(user=u, subscription=s)
 
     return us
+
 
 def handle_payment_was_successful(sender, **kwargs):
     us = _ipn_usersubscription(sender)
@@ -312,6 +343,7 @@ def handle_payment_was_successful(sender, **kwargs):
         signals.event.send(s, ipn=sender, subscription=s, user=u, event='unexpected_payment')
 ipn.signals.payment_was_successful.connect(handle_payment_was_successful)
 
+
 def handle_payment_was_flagged(sender, **kwargs):
     us = _ipn_usersubscription(sender)
     u, s = us.user, us.subscription
@@ -321,13 +353,15 @@ def handle_payment_was_flagged(sender, **kwargs):
     signals.event.send(s, ipn=sender, subscription=s, user=u, event='flagged')
 ipn.signals.payment_was_flagged.connect(handle_payment_was_flagged)
 
+
 def handle_subscription_signup(sender, **kwargs):
     us = _ipn_usersubscription(sender)
     u, s = us.user, us.subscription
     if us:
         # deactivate or delete all user's other subscriptions
         for old_us in u.usersubscription_set.all():
-            if old_us==us: continue     # don't touch current subscription
+            if old_us == us:
+                continue     # don't touch current subscription
             if old_us.cancelled:
                 old_us.delete()
                 Transaction(user=u, subscription=s, ipn=sender,
@@ -360,6 +394,7 @@ def handle_subscription_signup(sender, **kwargs):
                            event='unexpected_subscription')
 ipn.signals.subscription_signup.connect(handle_subscription_signup)
 
+
 def handle_subscription_cancel(sender, **kwargs):
     us = _ipn_usersubscription(sender)
     u, s = us.user, us.subscription
@@ -388,6 +423,7 @@ def handle_subscription_cancel(sender, **kwargs):
 ipn.signals.subscription_cancel.connect(handle_subscription_cancel)
 ipn.signals.subscription_eot.connect(handle_subscription_cancel)
 
+
 def handle_subscription_modify(sender, **kwargs):
     us = _ipn_usersubscription(sender)
     u, s = us.user, us.subscription
@@ -395,7 +431,8 @@ def handle_subscription_modify(sender, **kwargs):
     if us:
         # delete all user's other subscriptions
         for old_us in u.usersubscription_set.all():
-            if old_us==us: continue     # don't touch current subscription
+            if old_us == us:
+                continue     # don't touch current subscription
             old_us.delete()
             Transaction(user=u, subscription=s, ipn=sender,
                         event='remove subscription (deactivated)', amount=sender.mc_gross
